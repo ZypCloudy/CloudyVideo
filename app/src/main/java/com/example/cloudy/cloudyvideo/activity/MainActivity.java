f02b542fa0e121eef9be20912d70a097a93ba309
ZypCloudy <314705487@qq.com>
2017-11-06 21:53:31 +0800

    底部导航栏

--- app/src/main/java/com/example/cloudy/cloudyvideo/activity/MainActivity.java
+++ app/src/main/java/com/example/cloudy/cloudyvideo/activity/MainActivity.java
@@ -5,10 +5,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import com.example.cloudy.cloudyvideo.R;
 
 public class MainActivity extends Activity{
+    private FrameLayout fl_main_content;
+    private RadioGroup rg_buttom_tag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,9 @@ public class MainActivity extends Activity{
 //        textView.setTextColor(Color.BLUE);
 //        setContentView(textView);
         setContentView(R.layout.activity_main);
+        fl_main_content = (FrameLayout) findViewById(R.id.fl_main_content);
+        rg_buttom_tag = (RadioGroup) findViewById(R.id.rg_buttom_tag);
+
+        rg_buttom_tag.check(R.id.rb_recommend);
     }
 }
