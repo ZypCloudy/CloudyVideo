7842ced77f3760493c37e1893a8fc020b60d112b
ZypCloudy <314705487@qq.com>
2017-12-01 16:40:26 +0800

    导入xUtils

---
+++ app/src/main/java/com/example/cloudy/cloudyvideo/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.example.cloudy.cloudyvideo.utils;
+
+/**
+ * 作用：常量类，配置网络地址
+ */
+public class Constants {
+
+    /**
+     * 网络视频的联网地址
+     */
+    public static  final  String NET_URL = "http://api.m.mtime.cn/PageSubArea/TrailerList.api";
+}
