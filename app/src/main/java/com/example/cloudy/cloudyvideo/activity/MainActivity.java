8bbb69dd34253f8ca7a99528c56ebc2f48fcfbd2
ZypCloudy <314705487@qq.com>
2017-12-05 19:49:32 +0800

    配置修改

--- app/src/main/java/com/example/cloudy/cloudyvideo/activity/MainActivity.java
+++ app/src/main/java/com/example/cloudy/cloudyvideo/activity/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.cloudy.cloudyvideo.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import com.example.cloudy.cloudyvideo.R;
 import com.example.cloudy.cloudyvideo.base.BasePager;
